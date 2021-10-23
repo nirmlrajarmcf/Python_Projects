0077c5e361f81bd6d5ae428af98ca706529df11e
nirmlrajarmcf <nirmlrajarmcf@gmail.com>
2021-10-23 23:46:07 +0530

    Trying to include all files

--- Twitter_Task/START_APP.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-SET PATH=PATH;G:\NIRMAL_WORK\Job_Search\Venzo_Python_Task\Nirmal_Twitter_API\chromedriver.exe;C:\ProgramData\Anaconda3
-C:\Anaconda3\Scripts\ipython.exe "G:\NIRMAL_WORK\Job_Search\Venzo_Python_Task\Nirmal_Twitter_API\app_run.py"
-pause
